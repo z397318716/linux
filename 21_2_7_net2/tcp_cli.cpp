fc5b31798ba180e700bd26ddf27aafba022eafc4
z397318716 <397318716@qq.com>
2021-02-07 22:21:41 +0800

    net

---
+++ 21_2_7_net2/tcp_cli.cpp
@@ -0,0 +1,51 @@
+/*
+ * 通过封装的 tcpsocket类来实例化对象, 实现tcp 客户端通信*/
+#include<iostream>
+#include<stdlib.h>
+#include"tcp_socket.hpp"
+#include<signal.h>
+
+// 创建套接字
+// 绑定地址信息---推荐操作系统自己绑定
+// 向服务端发起请求
+// 循环收发数据
+void sigcb(int signo){
+
+  printf("连接断开, 继续发送数据触发异常SIGPIPE信号\n");
+}
+int main(int argc, char *argv[]){
+
+  if(argc != 3){
+    printf("em:./tcp_cli 192.168.132.122 9000--服务器绑定的地址\n");
+    return -1;
+  }
+  signal(SIGPIPE, sigcb);
+  std::string ip = argv[1];
+  //uint16_t port = std::stoi(argv[2]);
+  uint16_t port = atoi(argv[2]);
+  TcpSocket cli_sock;
+  // 创建套接字
+  CHECK_RET(cli_sock.Socket());
+  // 绑定地址信息---推荐让操作系统去做---能够最大化的避免端口冲突
+  // 向服务端发起请求
+  CHECK_RET(cli_sock.Connect(ip, port));
+  // 循环收发数据
+  while(1){
+    printf("client say: ");
+    fflush(stdout);
+    std::string buf;
+    std::cin >> buf;
+
+    // 因为客户端不存在多种套接字的问题, 因此一旦出现问题,直接退出
+    // 进程退出就会释放资源, 关闭套接字
+    CHECK_RET(cli_sock.Send(buf));
+
+    buf.clear();
+    CHECK_RET(cli_sock.Recv(&buf));
+    printf("server say: %s\n", buf.c_str());
+  }
+  cli_sock.Close();
+
+
+  return 0;
+}
---
+++ 21_2_7_net2/tcp_srv.cpp
@@ -0,0 +1,54 @@
+/*
+ * 使用封装的 TcpSocket 类实例化对象实现tcp服务端程序*/
+#include<iostream>
+#include<stdlib.h>
+#include"tcp_socket.hpp"
+
+
+int main(int argc, char *argv[]){
+
+  if(argc != 3){
+    printf("em:/tcp_srv 192.168.122.132 9000\n");
+    return -1;
+  }
+  std::string ip = argv[1];
+  //uint16_t port = std::stoi(argv[2]); // stoi 将字符串转换为数字
+  uint16_t port = atoi(argv[2]); // stoi 将字符串转换为数字
+
+  TcpSocket lst_sock;
+  CHECK_RET(lst_sock.Socket());
+  CHECK_RET(lst_sock.Bind(ip, port));
+  CHECK_RET(lst_sock.Listen()); // 开始监听
+
+  while(1){
+    TcpSocket cli_sock;
+    std::string cli_ip;
+    uint16_t cli_port;
+    // Accept 类成员函数, 使用的是私有成员 _sockfd就是lst_sock 对象的私有成员
+    // cli_sock 取地址传入, 目的是为了获取 accept 接口返回的通信套接字描述符
+    bool ret = lst_sock.Accept(&cli_sock, &cli_ip, &cli_port);
+    if(ret == false){
+      // 获取新连接失败, 重新获取
+      continue;
+    }
+    printf("new connect:[%s:%d]\n", cli_ip.c_str(), cli_port);
+    std::string buf;
+    if(cli_sock.Recv(&buf) == false){
+      cli_sock.Close();// 通信套接字接收数据出错, 关闭的是通信套接字
+      continue;
+    }
+    printf("client:[%s:%d] say:%s\n", &cli_ip[0], cli_port, &buf[0]);
+    std::cout << "server say: ";
+    fflush(stdout);
+    buf.clear();
+    std::cin >> buf;
+
+    if(cli_sock.Send(buf) == false){
+      cli_sock.Close();
+      continue;
+    }
+  }
+  lst_sock.Close();
+
+  return 0;
+}
